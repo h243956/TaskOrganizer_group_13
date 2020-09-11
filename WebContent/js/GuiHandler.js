26edce4e25ad062951f5c1b365ddb8cfad0488fb
Kenneth Kvamme <kenneth_kvamme@hotmail.com>
2020-09-11 23:22:08 +0200

    now puts new tasks first, and checks for duplicate ids

--- WebContent/js/GuiHandler.js
+++ WebContent/js/GuiHandler.js
@@ -1,24 +1,9 @@
 export default class GuiHandler {
   
-  constructor() {
+  constructor(container) {
+    this.container=container
     this.statuses=null
     this.prepareView()
-  }
-
-  prepareView() {
-    const tasklist = document.querySelector('.tasklist')
-    const tablenode = document.createElement("table");
-    tablenode.innerHTML = `
-      <thead>
-        <tr>
-          <th>Task</th>
-          <th colspan="3">Status</th>
-        </tr>
-      </thead>
-      <tbody class="tasks-table">
-      </tbody>
-    `;
-    tasklist.appendChild(tablenode)
   }
 
   set allStatuses(statuses) {
@@ -34,10 +19,52 @@ export default class GuiHandler {
   }
 
   showTask(task) {
-    const tasktable = document.querySelector(".tasks-table")
+    const tasktable = this.container.querySelector(".tasks-table")
+    const tasks = tasktable.children;
     const tasknode = document.createElement("tr");
     tasknode.setAttribute('data-key', task.id);
-    tasknode.innerHTML = `
+    tasknode.innerHTML = this.getTaskInnerHtml(task);
+    
+    if(tasks.length) {
+    	if(!this.isExistingTask(tasks, task)) {
+        tasktable.insertBefore(tasknode, tasks[0])
+      }
+    } else {
+    	tasktable.appendChild(tasknode)
+    }
+  }
+
+
+  update(task) {
+
+  }
+
+  removeTask(id) {
+
+  }
+
+  noTask() {
+
+  }
+
+  prepareView() {
+    const tasklist = this.container.querySelector('.tasklist')
+    const tablenode = document.createElement("table");
+    tablenode.innerHTML = `
+      <thead>
+        <tr>
+          <th>Task</th>
+          <th colspan="3">Status</th>
+        </tr>
+      </thead>
+      <tbody class="tasks-table">
+      </tbody>
+    `;
+    tasklist.appendChild(tablenode)
+  }
+
+  getTaskInnerHtml(task) {
+	  return `
       <tr data-identity="${task.id}">
         <td>${task.title}</td>
         <td>${task.status}</td>
@@ -55,22 +82,19 @@ export default class GuiHandler {
         </td>
         <td><button type="button">Remove</button></td>
       </tr>
-    `;
-    tasktable.appendChild(tasknode)
+  `;
   }
-
-  update(task) {
-
+  
+  isExistingTask(children, task) {
+    let childrenArr = Array.from(children)
+    let exists=false
+    childrenArr.forEach(element => {
+      if(task.id==element.getAttribute("data-key")) {
+        exists=true
+      }
+    })
+	  return exists
   }
-
-  removeTask(id) {
-
-  }
-
-  noTask() {
-
-  }
-
 
   
 }
--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,12 +1,12 @@
 import GuiHandler from './GuiHandler.js'
 
 const container = document.getElementById("taskcontainer")
-const gui = new GuiHandler()
+const gui = new GuiHandler(container)
 const statuses = ["WAITING","ACTIVE","DONE"]
 const tasks = [
     {"id":1,"title":"Paint roof","status":"WAITING"},
     {"id":2,"title":"Clean floor","status":"DONE"},
-    {"id":3,"title":"Wash windows","status":"ACTIVE"}
+    {"id":3,"title":"Wash windows","status":"ACTIVE"},
 ]
 
 gui.allStatuses = statuses
